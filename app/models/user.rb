a0b9d64c7e7e32147f93ab39df375e2490d7cf43
Malcolm Remple <hello@malcolmremple.com>
2019-11-26 14:13:35 -0500

    routes, models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :languages, through: :user_languages
-  has_many :meetups, as: :sender
-  has_many :meetups, as: :recipient
+  # has_many :meetups, as: :sender
+  # has_many :meetups, as: :recipient
+  
+  def meetups 
+    Meetup.where("sender_id = ? OR recipient_id = ?", id, id)
+  end
+  
+  def meetups_i_initiated
+    Meetup.where(sender_id: id)
+  end
 end
