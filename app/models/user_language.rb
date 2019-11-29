0d53cd76bb6184c3bc54e35a8687899f9c942291
Lynn Qi qilin2481@gmail.com <qilin2481@gmail.com>
2019-11-29 13:34:31 -0500

    changed seeds, make user index with languages and progress bar working

--- app/models/user_language.rb
+++ app/models/user_language.rb
@@ -1,4 +1,19 @@
 class UserLanguage < ApplicationRecord
   belongs_to :user
   belongs_to :language
+
+  def level
+      case proficiency
+    when 1
+      "20%"
+    when 2
+      "40%"
+    when 3
+      "60%"
+    when 4
+      "80%"
+    when 5
+      "100%"
+    end
+  end
 end
