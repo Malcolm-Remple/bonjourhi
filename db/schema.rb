49602611bb1d18a12d0cfb505dcd8d790b81d082
Malcolm Remple <hello@malcolmremple.com>
2019-12-02 11:13:40 -0500

    changed icons

--- db/schema.rb
+++ db/schema.rb
@@ -107,10 +107,30 @@ ActiveRecord::Schema.define(version: 2019_12_01_162628) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "vocab_lists", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "language_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["language_id"], name: "index_vocab_lists_on_language_id"
+    t.index ["user_id"], name: "index_vocab_lists_on_user_id"
+  end
+
+  create_table "vocabs", force: :cascade do |t|
+    t.string "item"
+    t.bigint "vocab_list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["vocab_list_id"], name: "index_vocabs_on_vocab_list_id"
+  end
+
   add_foreign_key "meetups", "languages", column: "seeking_lang_id"
   add_foreign_key "meetups", "languages", column: "sharing_lang_id"
   add_foreign_key "reviews", "users"
   add_foreign_key "reviews", "users", column: "author_id"
   add_foreign_key "user_languages", "languages"
   add_foreign_key "user_languages", "users"
+  add_foreign_key "vocab_lists", "languages"
+  add_foreign_key "vocab_lists", "users"
+  add_foreign_key "vocabs", "vocab_lists"
 end
