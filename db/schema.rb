07bc1d9b4947949b7162e686f25addaa2f9bb339
Lea Grelou <leagrelou@gmail.com>
2019-12-02 14:20:55 -0500

    ongoing google calendar feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_01_162628) do
+ActiveRecord::Schema.define(version: 2019_12_02_182048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 2019_12_01_162628) do
     t.text "bio"
     t.string "photo"
     t.integer "num_of_past_meetups"
+    t.integer "pending_event_confirmation"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
