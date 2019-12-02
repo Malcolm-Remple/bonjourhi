3568374077b0ce1e5f1f6bf9e947613f3ab7f4fd
claire froelich <claire.froelich@gmail.com>
2019-12-02 11:23:48 -0500

    organized js folder, added chat animation

---
+++ app/javascript/packs/plugins/chatScroll.js
@@ -0,0 +1,23 @@
+// get chatbox and set scroll height to bottom
+const scrollChatBox = () => {
+  const chatBox = document.getElementById("messages");
+  chatBox.scrollTop = chatBox.scrollHeight;
+};
+
+// scroll to bottom of chat on load
+const scrollToChatBottomOnLoad = () => {
+  window.onload = () => {
+    scrollChatBox();
+  }
+}
+// scroll to bottom of chat box on new message added
+const scrollToChatBottomOnMessageAppend = () => {
+  document.getElementById('messages').addEventListener('DOMNodeInserted', (e) => {
+    scrollChatBox();
+    // animate entrance
+    const new_message =  document.querySelector('#messages').lastChild;
+    new_message.classList.add('animated', 'slideInLeft', 'faster');
+  });
+};
+
+export { scrollToChatBottomOnLoad, scrollToChatBottomOnMessageAppend };
