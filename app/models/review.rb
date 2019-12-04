df7d1d52f2ee2e287b3d861f95585dffbc35dd37
Malcolm Remple <hello@malcolmremple.com>
2019-12-04 17:25:42 -0500

    fixed reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :author, foreign_key: :author_id, class_name: "User"
+  
+  def date_to_string
+    created_at.strftime("%d/%m/%Y")
+  end
 end
