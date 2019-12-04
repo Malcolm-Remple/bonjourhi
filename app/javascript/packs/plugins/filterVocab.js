c49d8d3127ab4487053336282d4e86e27f30841f
claire froelich <claire.froelich@gmail.com>
2019-12-04 11:59:20 -0500

    save

--- app/javascript/packs/plugins/filterVocab.js
+++ app/javascript/packs/plugins/filterVocab.js
@@ -3,27 +3,31 @@ const filterVocabByLang = () => {
 
   // get all vocab groups (by lang)
   const vocabItemLists = document.querySelectorAll('.vocab-item-list');
+  console.log(vocabItemLists);
 
-  vocabItemLists.forEach((vocabGroup)=> {
-    vocabGroup.style.display = 'none';
-  });
+  if(vocabItemList.length > 0) {
 
-  vocabItemLists[0].style.display = 'block';
-
-  console.log(vocabItemLists)
-
-  langSelector.addEventListener('change', (e) => {
-    const langId = e.currentTarget.value;
-
-    // loop trough lang groups and display none on all but current lang
-    Array.from(vocabItemLists).forEach((vocabGroup) => {
-      if(vocabGroup.id !== `language-${langId}`) {
-         vocabGroup.style.display = 'none';
-      } else {
-         vocabGroup.style.display = 'block';
-      }
+    vocabItemLists.forEach((vocabGroup)=> {
+      vocabGroup.style.display = 'none';
     });
-  });
+
+    vocabItemLists[0].style.display = 'block';
+
+    console.log(vocabItemLists)
+
+    langSelector.addEventListener('change', (e) => {
+      const langId = e.currentTarget.value;
+
+      // loop trough lang groups and display none on all but current lang
+      Array.from(vocabItemLists).forEach((vocabGroup) => {
+        if(vocabGroup.id !== `language-${langId}`) {
+           vocabGroup.style.display = 'none';
+        } else {
+           vocabGroup.style.display = 'block';
+        }
+      });
+    });
+  }
 }
 
 export { filterVocabByLang }
