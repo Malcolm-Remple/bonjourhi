5fd3c516e51301a18cba8fc25b762000572a57d0
Malcolm Remple <hello@malcolmremple.com>
2019-12-03 19:44:40 -0500

    added header title, login logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-  
+  before_action :set_title
   before_action :authenticate_user!
+
+
+  private
+  
+  def set_title
+    @header_title = ""
+  end
 end
