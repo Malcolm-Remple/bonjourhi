59184dda7af299fb0da3b8dcb960cb7a01a2ac47
claire froelich <claire.froelich@gmail.com>
2019-12-04 22:10:50 -0500

    more animation, button aligned

--- app/javascript/packs/plugins/filterVocab.js
+++ app/javascript/packs/plugins/filterVocab.js
@@ -1,16 +1,37 @@
+const addVocabListLoadAnimation = (element) => {
+  element.classList.add('animation');
+  element.classList.add('bounceIn');
+};
+
+const removeVocabListLoadAnimation = (element) => {
+  element.classList.remove('animation');
+  element.classList.remove('bounceIn');
+  element.classList.remove('flipInX');
+};
+
+
 const filterVocabByLang = () => {
-  const vocabItems = document.querySelectorAll('.vocab-item');
   const langSelector = document.getElementById('vocab_item_language_id');
 
+  // Array.from(vocabItems).forEach((vocabItem) => {
+  //   vocabItem.style.display = 'none';
+  // };
+
   langSelector.addEventListener('change', (e) => {
+  const vocabItems = document.querySelectorAll('.vocab-item');
     const langId = e.currentTarget.value;
+
 
     // loop trough lang groups and display none on all but current lang
     Array.from(vocabItems).forEach((vocabItem) => {
+      removeVocabListLoadAnimation(vocabItem);
+
       if(vocabItem.dataset.langId !== langId) {
          vocabItem.style.display = 'none';
+
       } else {
-         vocabItem.style.display = 'flex';
+        addVocabListLoadAnimation(vocabItem);
+        vocabItem.style.display = 'flex';
       }
     });
   });
