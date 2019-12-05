bfbfde8abea9009d45f8c8405bae307e23ceb0d7
Lea Grelou <leagrelou@gmail.com>
2019-12-05 13:43:02 -0500

    setup-demo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :set_title
   before_action :authenticate_user!
 
+  def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   private
 
   def set_title
